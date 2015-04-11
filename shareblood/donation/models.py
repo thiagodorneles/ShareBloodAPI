b35f5fb0737ca7b4c84f68ab0db70ceec6e745f3
Mauricio de Abreu Antunes <mauricio.abreua@gmail.com>
2015-04-11 18:44:07 -0300

    Ajustado um erro de sintaxe do commit anterior
    Ops de novo! :-)

--- shareblood/donation/models.py
+++ shareblood/donation/models.py
@@ -11,7 +11,7 @@ from shareblood import constants
 class Donation(models.Model):
     favored = models.CharField(null=True, blank=True, max_length=255)
     needed = models.IntegerField(null=True, blank=True)
-    donated = modelstart_date = models.DateField()s.IntegerField(null=True, blank=True)
+    donated = models.IntegerField(null=True, blank=True)
     start_date = models.DateField()
     end_date = models.DateField()
     description = models.CharField(max_length=140)
