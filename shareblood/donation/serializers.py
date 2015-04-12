81fe913b5cdf4efe2dd82d671ec6d27b750c5803
Mauricio de Abreu Antunes <mauricio.abreua@gmail.com>
2015-04-12 01:08:46 -0300

    Adicionada a relação no histórico
    Porém, ainda não funcionando. :(

--- shareblood/donation/serializers.py
+++ shareblood/donation/serializers.py
@@ -6,12 +6,16 @@ shareblood.donation.serializers
 from rest_framework import serializers
 from .models import Donation, DonationHistory
 
-class DonationSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Donation
-        fields = ('favored', 'needed', 'donated', 'start_date', 'end_date', 'description', 'owner', 'blood_bank', 'donation__history' )
-
 class DonationHistorySerializer(serializers.ModelSerializer):
     class Meta:
         model = DonationHistory
-        fields = ('teste')
+        fields = ('donator', )
+
+
+class DonationSerializer(serializers.ModelSerializer):
+    donations = DonationHistorySerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Donation
+        fields = ('favored', 'needed', 'donated', 'start_date', 'end_date',
+                  'description', 'owner', 'blood_bank', 'donations')
--- shareblood/donation/views.py
+++ shareblood/donation/views.py
@@ -5,12 +5,14 @@ from rest_framework.permissions import AllowAny
 from .models import Donation, DonationHistory
 from .serializers import DonationSerializer, DonationHistorySerializer
 
+
 class DonationView(viewsets.ModelViewSet):
     serializer_class = DonationSerializer
     model = Donation
 
     def get_queryset(self):
         return Donation.objects.all()
+
 
 class DonationHistoryView(viewsets.ModelViewSet):
     serializer_class = DonationHistorySerializer
