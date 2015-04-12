4ff228278bd3cfd0070efcb660288a97fadfc73c
Mauricio de Abreu Antunes <mauricio.abreua@gmail.com>
2015-04-11 22:24:46 -0300

    Removida obrigatoriedade campos admin de location

--- shareblood/location/models.py
+++ shareblood/location/models.py
@@ -8,11 +8,11 @@ from django.db import models
 from cities_light.models import City, Country, Region
 
 class Location(models.Model):
-    country = models.ForeignKey(Country, null=True)
-    state = models.ForeignKey(Region, null=True)
-    city = models.ForeignKey(City, null=True)
-    zipcode = models.CharField(null=True, max_length=255)
-    street = models.CharField(null=True, max_length=255)
-    neighborhood = models.CharField(null=True, max_length=255)
+    country = models.ForeignKey(Country, null=True, blank=True)
+    state = models.ForeignKey(Region, null=True, blank=True)
+    city = models.ForeignKey(City, null=True, blank=True)
+    zipcode = models.CharField(null=True, blank=True, max_length=255)
+    street = models.CharField(null=True, blank=True, max_length=255)
+    neighborhood = models.CharField(null=True, blank=True, max_length=255)
     latitude = models.BigIntegerField()
     longitude = models.BigIntegerField()
