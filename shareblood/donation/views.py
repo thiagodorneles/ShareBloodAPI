4c7f8dd475b66ffc26ed21798bfa33c8a95998e5
Thiago Dorneles <thiagodornelesrs@gmail.com>
2015-04-12 00:32:31 -0300

    Views para serializar objetos donations

--- shareblood/donation/views.py
+++ shareblood/donation/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework import permissions, viewsets
 
-# Create your views here.
+from rest_framework.permissions import AllowAny
+
+from .models import Donation, DonationHistory
+from .serializers import DonationSerializer, DonationHistorySerializer
+
+class DonationView(viewsets.ModelViewSet):
+    serializer_class = DonationSerializer
+    model = Donation
+
+    def get_queryset(self):
+        return Donation.objects.all()
+
+class DonationHistoryView(viewsets.ModelViewSet):
+    serializer_class = DonationHistorySerializer
+    model = DonationHistory
+
+    def get_queryset(self):
+        return DonationHistory.objects.all()
